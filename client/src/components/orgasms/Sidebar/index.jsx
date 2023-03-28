28b3e039130e03f7cadf8efda2cce7e0979dab64
RyotaHiraiwa <r-hiraiwa@qccs.co.jp>
2023-03-28 11:54:03 +0900

    [feat] サイドバー作成

---
+++ client/src/components/orgasms/Sidebar/index.jsx
@@ -0,0 +1,26 @@
+import './index.scss';
+
+const Sidebar = () => {
+  return (
+    <>
+      <nav>
+        <h2>logo</h2>
+        <ul>
+          <li className='headline'>管理</li>
+          <li>予算</li>
+          <li>立替</li>
+          <li className='headline'>入力</li>
+          <li>入金</li>
+          <li>支払</li>
+          <li className='headline'>照会</li>
+          <li>実績照会</li>
+          <li className='headline'>マスタメンテナンス</li>
+          <li>ユーザー</li>
+          <li>カテゴリ</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Sidebar;
