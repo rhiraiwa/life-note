e39f7cb070d398eb4664fe1a59b183eff87190ba
RyotaHiraiwa <r-hiraiwa@qccs.co.jp>
2023-03-30 13:26:58 +0900

    [update] 電卓ボタンデザイン修正

--- client/src/components/orgasms/Header/index.jsx
+++ client/src/components/orgasms/Header/index.jsx
@@ -1,11 +1,12 @@
 import FlexDiv from '../../atoms/FlexDiv';
+import calc from '../../../img/calc.png';
 import './index.scss';
 
 const Header = ({title}) => {
   return (
     <FlexDiv id='header'>
       <span>{title}</span>
-      <button>電卓</button>
+      <img src={calc} alt="calcrator"/>
     </FlexDiv>
   )
 }
