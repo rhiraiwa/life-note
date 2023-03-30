b440e338c89e095af812fc24c3dc7802a4bf3547
RyotaHiraiwa <r-hiraiwa@qccs.co.jp>
2023-03-30 11:43:13 +0900

    [feat] 立替管理画面デザイン実装

--- client/src/components/pages/AdvancesPaid/index.jsx
+++ client/src/components/pages/AdvancesPaid/index.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import FlexDiv from "../../atoms/FlexDiv";
 import LabelInput from "../../molecules/LabelInput";
+import YearMonthChanger from "../../molecules/YearMonthChanger";
+import './index.scss';
 
 const AdvancesPaid = () => {
 
-  const categorylist = [
-    {cd:'0', name: '食費'},
-    {cd:'1', name: '雑費'},
-    {cd:'2', name: '水道代'},
-    {cd:'3', name: '電気代'}
+  const adlist = [
+    {categary:'0', amount: '1500', shopName: 'AEON', date: '2023/2/23'},
+    {categary:'0', amount: '1500', shopName: 'AEON', date: '2023/2/23'},
+    {categary:'0', amount: '1500', shopName: 'AEON', date: '2023/2/23'},
+    {categary:'0', amount: '1500', shopName: 'AEON', date: '2023/2/23'}
   ]
 
   const userlist = [
@@ -15,49 +18,74 @@ const AdvancesPaid = () => {
     {cd: '1', name: 'user2'}
   ]
 
+  const date = new Date();
+  const [selected, setSelected] = React.useState({
+    year: date.getFullYear(),
+    month: date. getMonth()
+  })
+
+  const setAdvancesPaid = (advancesPaid) => {
+    document.getElementById('advances-paid_input').value = advancesPaid;
+  }
+
+  const clear = () => {
+    document.getElementById('advances-paid_input').value = '';
+  }
+
   return (
-    <div>
-      <div>
+    <div id='advances-paid'>
+      <div className='black-underline-2'>
         <FlexDiv>
-          <LabelInput label='年' type='text'/>
-          <LabelInput label='月' type='text'/>
-          <label>ユーザー</label>
-          <select>
-            {
-              userlist.map((user) => (
-                <option value={user.cd}>{user.name}</option>
-              ))
-            }
-          </select>
+          <YearMonthChanger state={{selected, setSelected}}/>
+          <div>
+            <label>ユーザー</label>
+            <select>
+              {
+                userlist.map((user) => (
+                  <option value={user.cd}>{user.name}</option>
+                ))
+              }
+            </select>
+          </div>
         </FlexDiv>
-        <FlexDiv>
-          <LabelInput label='立替額合計' type='text' isReadOnly={true}/>
-          <button>一括返金登録</button>
+        <FlexDiv id='bulk-return-area'>
+          <LabelInput id='bulk-sum' label='立替額合計' type='text' isReadOnly={true}/>
+          <button className='button-primary'>一括返金登録</button>
         </FlexDiv>
       </div>
-      <FlexDiv>
-        <div>
+      <FlexDiv id='return-area'>
+        <div id='advances-paid-list'>
           <span>立替一覧</span>
           <table>
             <thead>
               <tr>
-                <th>カテゴリ</th>
-                <th>立替額</th>
-                <th>立替日</th>
+                <th className='col-category'>カテゴリ</th>
+                <th className='col-amount'>立替額</th>
+                <th className='col-shop-name'>店名</th>
+                <th className='col-date'>立替日</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-              </tr>
+              {
+                adlist.map((history) => (
+                  <tr onClick={()=>setAdvancesPaid(history.amount)}>
+                    <td className='col-category'>{history.categary}</td>
+                    <td className='col-amount'>{history.amount}</td>
+                    <td className='col-shop-name'>{history.shopName}</td>
+                    <td className='col-date'>{history.date}</td>
+                  </tr>                  
+                ))
+              }
+
             </tbody>
           </table>
         </div>
         <div>
-          <LabelInput label='立替額' type='text' isReadOnly={true}/>
-          <button>返金登録</button>
+          <LabelInput id='advances-paid' label='立替額' type='text' isReadOnly={true}/>
+          <FlexDiv>
+            <button className='button-primary'>返金登録</button>
+            <button className='button-cancel' onClick={clear}>キャンセル</button>
+          </FlexDiv>
         </div>
       </FlexDiv>
     </div>
