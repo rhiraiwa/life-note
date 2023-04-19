3c5585c6e0c04b9773f3ee916071bf437ff5a7a5
RyotaHiraiwa <r-hiraiwa@qccs.co.jp>
2023-04-19 13:11:55 +0900

    [update] ユーザー・カテゴリ未登録時の挙動を実装

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,12 @@ import { useMasterFileData } from "./context/MasterFileContext";
 
 function App() {
 
-  const {setUserlist, setCategorylist} = useMasterFileData();
+  const {userlist, setUserlist, categorylist, setCategorylist} = useMasterFileData();
+
   React.useEffect(() => {
+    console.log(userlist);
+    console.log(categorylist);
+    console.log('list取得')
     fetch('http://localhost:5000/category_and_user_select', { method: 'POST' })
     .then(response => response.json())
     .then(json => {
@@ -22,49 +26,62 @@ function App() {
       setUserlist(JSON.parse(json['user']))
     })
     .catch(err => alert(err))
-  }, []);
+  },[]);
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={
-          <PageTemplate title='ホーム'>
-            <Main/>
-          </PageTemplate>
-        }/>
-        <Route path='/Budget' element={
-          <PageTemplate title='予算管理'>
-            <Budget/>
-          </PageTemplate>
-        }/>
-        <Route path='/Deposit' element={
-          <PageTemplate title='入金入力'>
-            <Deposit/>
-          </PageTemplate>
-        }/>
-        <Route path='/Payment' element={
-          <PageTemplate title='支払入力'>
-            <Payment/>
-          </PageTemplate>
-        }/>
-        <Route path='/AdvancesPaid' element={
-          <PageTemplate title='立替管理'>
-            <AdvancesPaid/>
-          </PageTemplate>
-        }/>
-        <Route path='/Result' element={
-          <PageTemplate title='実績照会'>
-            <Result/>
+        {
+          userlist.length !== 0 && categorylist.length !== 0 &&
+          <>
+            <Route path='/' element={
+              <PageTemplate title='ホーム'>
+                <Main/>
+              </PageTemplate>
+            }/>
+            <Route path='/Budget' element={
+              <PageTemplate title='予算管理'>
+                <Budget/>
+              </PageTemplate>
+            }/>
+            <Route path='/Deposit' element={
+              <PageTemplate title='入金入力'>
+                <Deposit/>
+              </PageTemplate>
+            }/>
+            <Route path='/Payment' element={
+              <PageTemplate title='支払入力'>
+                <Payment/>
+              </PageTemplate>
+            }/>
+            <Route path='/AdvancesPaid' element={
+              <PageTemplate title='立替管理'>
+                <AdvancesPaid/>
+              </PageTemplate>
+            }/>
+            <Route path='/Result' element={
+              <PageTemplate title='実績照会'>
+                <Result/>
+              </PageTemplate>
+            }/>
+          </>
+        }
+        {
+          userlist.length === 0 && categorylist.length === 0 &&
+          <Route path='/' element={
+            <PageTemplate title='ユーザーマスタメンテナンス'>
+              <UserMaintenance/>
+            </PageTemplate>
+          }/>
+        }
+        <Route path='/UserMaintenance' element={
+          <PageTemplate title='ユーザーマスタメンテナンス'>
+            <UserMaintenance/>
           </PageTemplate>
         }/>
         <Route path='/CategoryMaintenance' element={
           <PageTemplate title='カテゴリマスタメンテナンス'>
             <CategoryMaintenance/>
-          </PageTemplate>
-        }/>
-        <Route path='/UserMaintenance' element={
-          <PageTemplate title='ユーザーマスタメンテナンス'>
-            <UserMaintenance/>
           </PageTemplate>
         }/>
       </Routes>
--- client/src/components/orgasms/Sidebar/index.jsx
+++ client/src/components/orgasms/Sidebar/index.jsx
@@ -1,20 +1,29 @@
+import { useMasterFileData } from '../../../context/MasterFileContext';
 import './index.scss';
 
 const Sidebar = () => {
+
+  const {userlist, categorylist} = useMasterFileData();
+
   return (
     <>
       <nav>
         <h2><a href='/'>APP Title</a></h2>
         <ul>
-          <li><a href='/'>ホーム</a></li>
-          <li className='headline'>管理</li>
-          <li><a href='/Budget'>予算</a></li>
-          <li><a href='/AdvancesPaid'>立替</a></li>
-          <li className='headline'>入力</li>
-          <li><a href='/Deposit'>入金</a></li>
-          <li><a href='/Payment'>支払</a></li>
-          <li className='headline'>照会</li>
-          <li><a href='/Result'>実績</a></li>
+          {
+            userlist.length !== 0 && categorylist.length !== 0 &&
+            <>
+              <li><a href='/'>ホーム</a></li>
+              <li className='headline'>管理</li>
+              <li><a href='/Budget'>予算</a></li>
+              <li><a href='/AdvancesPaid'>立替</a></li>
+              <li className='headline'>入力</li>
+              <li><a href='/Deposit'>入金</a></li>
+              <li><a href='/Payment'>支払</a></li>
+              <li className='headline'>照会</li>
+              <li><a href='/Result'>実績</a></li>
+            </>
+          }
           <li className='headline'>メンテナンス</li>
           <li><a href='/UserMaintenance'>ユーザー</a></li>
           <li><a href='/CategoryMaintenance'>カテゴリ</a></li>
